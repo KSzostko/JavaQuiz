11805063425e8df39cd8e37c49d770d4b51997e1
Kuba Szostko <kubaszostko@gmail.com>
2020-10-29 18:32:23 +0100

    created View and Question classes

---
+++ src/com/company/View.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public abstract class View {
+    public abstract void displayStartView();
+    public abstract void displayLeadersView();
+    public abstract void displayQuestionView(Question question);
+    public abstract void displayEndView();
+}
