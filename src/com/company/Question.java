ac5742e68359de9fb9c4307acc8d22fec6efd8ff
Kuba Szostko <kubaszostko@gmail.com>
2020-10-30 12:05:22 +0100

    added fields and constructor to the Question class

--- src/com/company/Question.java
+++ src/com/company/Question.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Question {
+    private String questionText;
+    private String[] answers;
+    private int correctAnswer;
+
+    public Question(String questionText, String[] answers, int correctAnswer) {
+        this.questionText = questionText;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
 }
