a3f27bba79acfefc96b14f94b2c6557906cb547f
Kuba Szostko <kubaszostko@gmail.com>
2020-10-30 12:00:25 +0100

    created simple StartView

--- src/com/company/TextView.java
+++ src/com/company/TextView.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
@@ -16,17 +18,80 @@ public class TextView extends View {
         Terminal terminal = null;
         try {
             terminal = defaultTerminalFactory.createTerminal();
+            TerminalSize terminalSize = terminal.getTerminalSize();
             final TextGraphics textGraphics = terminal.newTextGraphics();
 
             terminal.enterPrivateMode();
             terminal.clearScreen();
 
-            textGraphics.putString(2, 1, "Siema witaj w quizie");
+            String welcomeMessage = "Welcome to the Quiz App!";
+            String hintMessage = "Input red characters to select given option";
+            String newGame = "New Game";
+            String leaderboard = "Leaderboards";
+            String exit = "Exit";
+
+            textGraphics.putString(terminalSize.getColumns() / 2 - welcomeMessage.length() / 2, 5, welcomeMessage);
+
+            terminal.setCursorPosition(terminalSize.getColumns() / 2 - newGame.length() / 2, 8);
+            terminal.setForegroundColor(TextColor.ANSI.RED);
+            terminal.putCharacter('N');
+            terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
+            terminal.putCharacter('e');
+            terminal.putCharacter('w');
+            terminal.putCharacter(' ');
+            terminal.putCharacter('G');
+            terminal.putCharacter('a');
+            terminal.putCharacter('m');
+            terminal.putCharacter('e');
+
+            terminal.setCursorPosition(terminalSize.getColumns() / 2 - leaderboard.length() / 2, 10);
+            terminal.setForegroundColor(TextColor.ANSI.RED);
+            terminal.putCharacter('L');
+            terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
+            terminal.putCharacter('e');
+            terminal.putCharacter('a');
+            terminal.putCharacter('d');
+            terminal.putCharacter('e');
+            terminal.putCharacter('r');
+            terminal.putCharacter('b');
+            terminal.putCharacter('o');
+            terminal.putCharacter('a');
+            terminal.putCharacter('r');
+            terminal.putCharacter('d');
+            terminal.putCharacter('s');
+
+            terminal.setCursorPosition(terminalSize.getColumns() / 2 - exit.length() / 2, 12);
+            terminal.setForegroundColor(TextColor.ANSI.RED);
+            terminal.putCharacter('E');
+            terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
+            terminal.putCharacter('x');
+            terminal.putCharacter('i');
+            terminal.putCharacter('t');
+
+            textGraphics.putString(terminalSize.getColumns() / 2 - hintMessage.length() / 2, 15, hintMessage);
             terminal.flush();
 
             KeyStroke keyStroke = terminal.readInput();
-            while(keyStroke.getKeyType() != KeyType.Escape) {
+            while(keyStroke.getKeyType() != KeyType.Escape && keyStroke.getKeyType() != KeyType.EOF
+                    && keyStroke.getCharacter() != 'e' && keyStroke.getCharacter() != 'E'
+                    && keyStroke.getCharacter() != 'n' && keyStroke.getCharacter() != 'N'
+                    && keyStroke.getCharacter() != 'l' && keyStroke.getCharacter() != 'L') {
                 keyStroke = terminal.readInput();
+            }
+
+            // this check can be done in game class
+            switch(keyStroke.getCharacter()) {
+                case 'n':
+                case 'N':
+                    // new game screen
+                    break;
+                case 'l':
+                case 'L':
+                    // leaderboard screen
+                    break;
+                case 'e':
+                case 'E':
+                    // exit game
             }
 
             terminal.flush();
