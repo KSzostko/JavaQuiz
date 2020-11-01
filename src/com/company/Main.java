5b5ff959927363cf7f102f38e5627ced33420f32
Kuba Szostko <kubaszostko@gmail.com>
2020-11-01 09:28:34 +0100

    created test class for Quiz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 //        TextView textView = new TextView();
 //        textView.displayStartView();
-        Quiz quiz = new Quiz("test");
+        Quiz quiz = new Quiz("com/company/test");
         System.out.println(quiz.loadQuestions());
         System.out.println(quiz);
     }
---
+++ src/com/company/test/QuizTest.java
@@ -0,0 +1,11 @@
+package com.company.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuizTest {
+
+    @org.junit.jupiter.api.Test
+    public void loadQuestions() {
+        fail("This test has yet to be implemented");
+    }
+}
