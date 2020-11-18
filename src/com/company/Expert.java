d1ec9d9b23dc6c5342b4d67bd5dd1ccfe52e3e52
Kuba Szostko <kubaszostko@gmail.com>
2020-11-18 16:59:12 +0100

    created Expert class

---
+++ src/com/company/Expert.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Expert {
+    private final List<String> expertAnswers = new ArrayList<>();
+
+    public void Expert() {
+        populateAnswers();
+    }
+
+    private void populateAnswers() {
+        expertAnswers.add("This is just trivial! It is of course ");
+        expertAnswers.add("This must be ");
+        expertAnswers.add("Why do you bother me with such an easy question? Everybody knows that the answer is ");
+        expertAnswers.add("Oh man this is a tough one. I guess the most sensible seems ");
+        expertAnswers.add("I'm sure the right answer is ");
+        expertAnswers.add("Honestly I have no idea. If I were you, I would try ");
+        expertAnswers.add("I can't quite remember it, but I would go with ");
+        expertAnswers.add("If you were present on the last lecture, you would surely know that the right answer is ");
+        expertAnswers.add("I bet the right answer is ");
+        expertAnswers.add("I am no expert in this area, but I would risk with the answer ");
+    }
+
+    public String chooseExpertAnswer() {
+        Random random = new Random();
+        int answerId = random.nextInt(expertAnswers.size());
+
+        return expertAnswers.get(answerId);
+    }
+}
