c8d840343d3cd1ea4d567d983180e92ea9af3cb2
Kuba Szostko <kubaszostko@gmail.com>
2020-12-04 09:43:24 +0100

    removed unnecessary comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        Leaderboard leaderboard = new Leaderboard();
-//        leaderboard.addScore(new Score("Ktos", "Test", 20000));
-//        leaderboard.addScore(new Score("asd", "nowy", 40000));
-//        leaderboard.addScore(new Score("asd", "nowy", 25000));
-        TextView textView = new TextView();
-//        Question question = new Question("TEST", new String[] { "tesa", "tesaaaa", "aaaaaaaaaaaaaaaaaaaaaaa", "cos" }, 0);
-//        textView.displayQuestionView(question);
-//        textView.displayLeadersView();
-//        textView.displayStartView();
-//        textView.displayEndView(new Score("asd", "nowy", 25000));
-//        Quiz.getTypes();
+        View textView = new TextView();
     }
 }
