4a12599865851e150ede8fcf29e2c6a1daaf3eca
Kuba Szostko <kubaszostko@gmail.com>
2020-12-10 13:17:13 +0100

    created code for start view

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,74 +1,81 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Main {
 
     public static void main(String[] args) {
         //View textView = new TextView();
 
-        Color BUTTON_COLOUR1 = Color.WHITE;
-        int BUTTON_HEIGHT = 75;
-        int BUTTON_WIDTH = 200;
-        final int TEXTFIELD_HEIGHT = 400;
-        int TEXTFIELD_WIDTH = 50;
-        String SECONDS_PER_MINUTE = "Seconds to Minutes or Minutes to Seconds";
-        String POUNDS_PER_KILOGRAM = "Pounds to Kilograms or Kilograms to Pounds";
-        int WIDTH = 400;
-        int HEIGHT = 400;
+        JFrame mainFrame;
+        JLabel headerLabel;
+        JPanel controlPanel;
 
-        // instance fields
-        JButton button1;
-        JButton button2;
-        JFrame frame;
-        JTextField textInput;
-        JPanel panel;
+        mainFrame = new JFrame("Java SWING Examples");
+        mainFrame.setSize(600,500);
+        mainFrame.setLayout(new BorderLayout(20, 60));
+        mainFrame.getContentPane().setBackground(Color.decode("#FFFFFF"));
 
-        frame = new JFrame("Centering example");
-        panel = new JPanel();
-        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        headerLabel = new JLabel("",JLabel.CENTER);
 
-        // Establish button dimensions.
-        Dimension buttonDimension = new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT);
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
 
-        // Establish Textfield dimensions.
-        Dimension textDimension = new Dimension(TEXTFIELD_HEIGHT, TEXTFIELD_WIDTH);
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+        controlPanel.setBackground(Color.WHITE);
 
-        // Create and add the first button.
-        button1 = new JButton(SECONDS_PER_MINUTE);
-        button1.setPreferredSize(buttonDimension);
-        button1.setMinimumSize(buttonDimension);
-        button1.setMaximumSize(buttonDimension);
-        button1.setBackground(BUTTON_COLOUR1);
-        panel.add(button1);
+        mainFrame.add(headerLabel, BorderLayout.PAGE_START);
+        mainFrame.add(controlPanel, BorderLayout.CENTER);
+        mainFrame.setVisible(true);
 
-        // Create and add the second button.
-        button2 = new JButton(POUNDS_PER_KILOGRAM);
-        button2.setPreferredSize(buttonDimension);
-        button2.setMinimumSize(buttonDimension);
-        button2.setMaximumSize(buttonDimension);
-        button2.setBackground(BUTTON_COLOUR1);
-        panel.add(button2);
+        headerLabel.setText("Welcome to the Quiz app!");
+        headerLabel.setFont(new Font("Lato", Font.BOLD, 20));
+        Border border = headerLabel.getBorder();
+        Border margin = new EmptyBorder(50, 10, 10, 10);
+        headerLabel.setBorder(new CompoundBorder(border, margin));
 
-        // Create an input text field.
-        textInput = new JTextField(20);
-        textInput.setPreferredSize(textDimension);
-        textInput.setMinimumSize(textDimension);
-        textInput.setMaximumSize(textDimension);
-        textInput.setHorizontalAlignment(JTextField.CENTER);
+        JButton okButton = new JButton("New Game");
+        okButton.setBackground(Color.decode("#6200EE"));
+        okButton.setForeground(Color.decode("#FFFFFF"));
+        okButton.setBorderPainted(false);
+        okButton.setFont(new Font("Lato", Font.PLAIN, 15));
+        okButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        panel.add(textInput, BorderLayout.CENTER);
-        String string = textInput.getText();
+        JButton submitButton = new JButton("Leaderboard");
+        submitButton.setBackground(Color.decode("#6200EE"));
+        submitButton.setForeground(Color.decode("#FFFFFF"));
+        submitButton.setBorderPainted(false);
+        submitButton.setFont(new Font("Lato", Font.PLAIN, 15));
+        submitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        frame.add(panel);
-        // Display the frame and text field.
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        JButton cancelButton = new JButton("Exit");
+        cancelButton.setBackground(Color.decode("#6200EE"));
+        cancelButton.setForeground(Color.decode("#FFFFFF"));
+        cancelButton.setBorderPainted(false);
+        cancelButton.setFont(new Font("Lato", Font.PLAIN, 15));
+        cancelButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        controlPanel.add(okButton);
+        // spacing between objects
+        controlPanel.add(Box.createVerticalStrut(20));
+
+        controlPanel.add(submitButton);
+        controlPanel.add(Box.createVerticalStrut(20));
+
+        controlPanel.add(cancelButton);
+
+        mainFrame.setVisible(true);
     }
 }
